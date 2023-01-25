cfbce550a86e4d8f4f65c8d70bde1c1887d73c98
Carles Galan <galan.carles@gmail.com>
2023-01-25 21:22:07 +0100

    Create header component

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Header } from "./components/HeaderComponent/Header";
+
+const container = document.querySelector(".container")!;
+const headerTitle = "My Series";
+const header = new Header(container, headerTitle);
