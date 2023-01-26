b1ab6af72b135c613dfb3ccc8e3b32f4ce0631d1
Anna Marrodán <anna.marrodan@gmail.com>
2023-01-26 22:58:14 +0100

    Insert images and titles to CardComponents

--- src/components/Components/CardComponent/CardComponent.ts
+++ src/components/Components/CardComponent/CardComponent.ts
@@ -1,30 +1,18 @@
+import { series } from "../../../series";
 import { Component } from "../Component";
 
 export class CardComponent extends Component {
   render() {
     super.render();
-    this.element.innerHTML = `
-          
-            <img class="serie__poster"
-              src="https://m.media-amazon.com/images/M/MV5BZGJjYzhjYTYtMDBjYy00OWU1LTg5OTYtNmYwOTZmZjE3ZDdhXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg"
-              alt="The Sopranos poster" />
-            <h4 class="serie__title">The Sopranos</h4>
-            <span class="serie__info">David Chase (1999)</span>
-            <ul class="score">
-              <li class="score__star">
-                <button><i class="icon icon--score far fa-star" title="1/5"></i></button>
-              </li>
-              <li class="score__star">
-                <button><i class="icon icon--score far fa-star" title="2/5"></i></button>
-              </li>
-              <li class="score__star">
-                <button><i class="icon icon--score far fa-star" title="3/5"></i></button>
-              </li>
-              <li class="score__star">
-                <button><i class="icon icon--score far fa-star" title="4/5"></i></button>
-              </li>
-              <li class="score__star">
-                <button><i class="icon icon--score far fa-star" title="5/5"></i></button>
-            `;
+    this.element.innerHTML = `${series
+      .map(
+        (serie) =>
+          `<img
+          class="serie__poster"
+          src="${serie.poster}"
+          alt="${serie.name} poster"
+        />`
+      )
+      .join("")}`;
   }
 }
