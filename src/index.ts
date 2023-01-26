6aedde162e96426dbdbc460ce0ee577755ab4945
Carles Galan <galan.carles@gmail.com>
2023-01-26 19:57:36 +0100

    Create pending series section

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { PageComponent } from "./components/Components/PageComponent/PageComponent";
+import { SectionComponent } from "./components/Components/SectionComponent/SectionComponent";
 
 const container = document.body;
 
 const header = new PageComponent(container, "container", "div");
 header.render();
+
+const section = new SectionComponent(header.element, "list", "section");
+section.render();
