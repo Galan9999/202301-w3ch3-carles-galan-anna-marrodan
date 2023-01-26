e98a0ba102f5940eb1673f124722985fa64b5f99
Carles Galan <galan.carles@gmail.com>
2023-01-26 21:22:06 +0100

    Create cardList and CardComponent

--- src/components/types.ts
+++ src/components/types.ts
@@ -2,3 +2,16 @@ export interface ComponentStructure {
   element: Element;
   render: () => void;
 }
+
+export interface SeriesStructure {
+  id: number;
+  name: string;
+  creator: string;
+  year: number;
+  poster: string;
+  isWatched: boolean;
+  score: 0 | 1 | 2 | 3 | 4 | 5;
+  emmies: number;
+}
+
+export type Series = SeriesStructure[];
